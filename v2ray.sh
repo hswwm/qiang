8ead323464b08540f8dd1842d248b74a81702bef
Mr.H <823527728@qq.com>
2020-08-11 11:14:07 +0800

    Add files via upload

---
+++ v2ray.sh
@@ -0,0 +1,90 @@
+echo 'input domain'
+read your_domain
+echo 'input email'
+read your_email
+your_uuid=`uuid`
+
+sudo echo "deb [trusted=yes] https://apt.fury.io/caddy/ /" \
+    | sudo tee -a /etc/apt/sources.list.d/caddy-fury.list
+sudo apt update
+sudo apt install caddy 
+curl -L -s https://install.direct/go.sh -o go.sh
+sudo chmod +x ./go.sh
+sudo ./go.sh 
+
+sudo mkdir /ray
+sudo mkdir /var/log/caddy
+sudo touch /var/log/caddy/caddy.log
+sudo chown -R caddy:caddy /var/log/caddy
+sudo chown -R root:caddy /etc/caddy
+sudo mkdir /etc/ssl/caddy
+sudo chown -R caddy:root /etc/ssl/caddy
+sudo chmod 0770 /etc/ssl/caddy
+sudo mkdir /var/www
+sudo mkdir /var/www/${your_domain}
+sudo touch /var/www/${your_domain}/index.html
+sudo chown -R caddy:caddy /var/www
+sudo touch /etc/caddy/Caddyfile
+
+cat > /etc/caddy/Caddyfile <<-EOF
+${your_domain}
+log {
+output file /var/log/caddy/caddy.log
+}
+tls ${your_email}
+root * /var/www/${your_domain}
+file_server
+@websockets {
+header Connection Upgrade
+header Upgrade websocket
+}
+reverse_proxy @websockets --from /ray --to 127.0.0.1:2589
+EOF
+
+cat > /var/www/${your_domain}/index.html <<-EOF
+<h>working...</h>
+EOF
+
+cat > /etc/v2ray/config.json <<-EOF
+{
+  "inbounds": [
+    {
+      "port": 2589,
+      "listen":"127.0.0.1",
+      "protocol": "vmess",
+      "settings": {  
+        "clients": [
+          {
+            "id": "${your_uuid}",
+            "alterId": 64
+          }
+        ]
+      },
+      "streamSettings": {
+        "network": "ws",
+        "wsSettings": {
+        "path": "/ray"
+        }
+      }
+    }
+  ],
+  "outbounds": [
+    {
+      "protocol": "freedom",
+      "settings": {}
+    }
+  ]
+}
+EOF
+
+sudo systemctl enable caddy.service
+sudo systemctl enable v2ray.service
+sudo caddy run --config /etc/caddy/Caddyfile
+sudo systemctl start v2ray.service
+
+wget --no-check-certificate https://github.com/teddysun/across/raw/master/bbr.sh
+sudo chmod +x bbr.sh
+sudo ./bbr.sh
+
+echo 'your uuid:'
+echo ${your_uuid}
